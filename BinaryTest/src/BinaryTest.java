0ba46113a2fa94f29a8f7963c26c06860ff3886e
awdf4 <awdf4@DESKTOP-DIDF0GS>
2021-11-26 14:15:18 +0900

    first commit

---
+++ BinaryTest/src/BinaryTest.java
@@ -0,0 +1,15 @@
+
+public class BinaryTest {
+	public static void main(String[] args) {
+		
+		int Num = 10; 		// 10진수
+		int bNum = 0B0101;	// 2진수
+		int oNum = 012;		// 8진수
+		int xNum = 0XA;		// 12진수
+		
+		System.out.println(Num);
+		System.out.println(bNum);
+		System.out.println(oNum);
+		System.out.println(xNum);
+	}
+}
